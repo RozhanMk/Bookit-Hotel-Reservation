8d75ec9a4c303ea0cb4afca48064a58f6713ae24
RozhanMK <rozhan.mirzaei82@gmail.com>
2025-06-05 01:22:31 +0330

    change admin in hotel

--- hotel/admin.py
+++ hotel/admin.py
@@ -39,7 +39,7 @@ class HotelAdmin(admin.ModelAdmin):
 
     def image_tag(self, obj):
         if obj.image:
-            url = f'http://{SERVER_URL}{MEDIA_URL}{obj.image.name}'
+            url = f'{SERVER_URL}{MEDIA_URL}{obj.image.name}'
             return format_html(
                 '<a href="{}" target="_blank"><img src="{}" width="80" height="85" /></a>',
                 url,
@@ -49,7 +49,7 @@ class HotelAdmin(admin.ModelAdmin):
 
     def hotel_license_tag(self, obj):
         if obj.hotel_license:
-            url = f'http://{SERVER_URL}{MEDIA_URL}{obj.hotel_license.name}'
+            url = f'{SERVER_URL}{MEDIA_URL}{obj.hotel_license.name}'
             return format_html(
                 '<a href="{}" target="_blank"><img src="{}" width="80" height="85" /></a>',
                 url,
