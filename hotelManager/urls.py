91a7431f447e0cf2cfab842233cb17be9bbadc0e
RozhanMK <rozhan.mirzaei82@gmail.com>
2025-06-11 17:16:22 +0330

    update url

--- hotelManager/urls.py
+++ hotelManager/urls.py
@@ -5,16 +5,14 @@ from django.urls import path
 urlpatterns = [
     path('hotel-managers/', HotelManagerViewSet.as_view({
         'get': 'list',
-        'post': 'create'
     })),
     path('hotel-managers/<int:pk>/', HotelManagerViewSet.as_view({
         'get': 'retrieve',
-        'put': 'update',
         'patch': 'partial_update',
         'delete': 'destroy'
     })),
     path('hotel-managers/profile/', HotelManagerViewSet.as_view({
-        'get': 'get_profile'  # would need to implement this method
+        'get': 'get_profile'  
     })),
     # Your non-auth endpoints
     path('register/', NoneAuthHotelManagerViewSet.as_view({'post': 'create'})),
