89524237412f45b10bf23f40bb6434de1b29d8d4
RozhanMK <rozhan.mirzaei82@gmail.com>
2025-06-11 12:54:03 +0330

    test

--- hotelManager/urls.py
+++ hotelManager/urls.py
@@ -7,10 +7,25 @@ from drf_yasg import openapi
 
 
 urlpatterns = [
-    path('hotel-manager/', HotelManagerViewSet.as_view({'patch': 'partial_update', 'get': 'retrieve'})),
-    path('get/', NoneAuthHotelManagerViewSet.as_view({'post': 'retrieve'})),
-    path('remove/', HotelManagerViewSet.as_view({'delete': 'destroy',})),
-    path('create/', NoneAuthHotelManagerViewSet.as_view({'post': 'create',})),
-    path('list/', HotelManagerViewSet.as_view({'get': 'list',})),
-]
+    # Authenticated Hotel Manager actions
+    path('hotel-manager/', HotelManagerViewSet.as_view({
+        'patch': 'partial_update',
+    }), name='hotel-manager-update'),
 
+    path('delete/', HotelManagerViewSet.as_view({
+        'delete': 'destroy',
+    }), name='hotel-manager-delete'),
+
+    path('hotel-managers/', HotelManagerViewSet.as_view({
+        'get': 'list',
+    }), name='hotel-manager-list'),
+
+    # Unauthenticated actions
+    path('register/', NoneAuthHotelManagerViewSet.as_view({
+        'post': 'create',
+    }), name='hotel-manager-register'),
+
+    path('login/', NoneAuthHotelManagerViewSet.as_view({
+        'post': 'retrieve',  
+    }), name='hotel-manager-login'),
+]
