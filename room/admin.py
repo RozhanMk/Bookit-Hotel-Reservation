e995f5c2a843004b7153eccdaab37e35146648db
RozhanMK <rozhan.mirzaei82@gmail.com>
2025-07-15 12:12:24 +0330

    add migrations

--- room/admin.py
+++ room/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from bookit.settings import SERVER_IP, SERVER_PORT, MEDIA_URL
+from bookit.settings import SERVER_URL, MEDIA_URL
 from django.utils.html import format_html
 from .models import Room, RoomLock, RoomType, DiscountStatus
 from django.utils import timezone
@@ -57,7 +57,7 @@ class RoomAdmin(admin.ModelAdmin):
 
     def image_tag(self, obj):
         if obj.image:
-            url = f'http://{SERVER_IP}:{SERVER_PORT}{MEDIA_URL}{obj.image.name}'
+            url = f'{SERVER_URL}{MEDIA_URL}{obj.image.name}'
             return format_html(
                 '<a href="{}" target="_blank"><img src="{}" width="80" height="85" /></a>',
                 url,
